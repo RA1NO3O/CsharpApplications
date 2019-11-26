a1addfed03acd80c36e2adcc2c83d3cc38350332
RA1N <RA1NN1AR@users.noreply.github.com>
2019-11-26 10:53:28 +0800

    Nov26th

---
+++ StudentScore/StudentScore/Form1.Designer.cs
@@ -0,0 +1,123 @@
+﻿namespace StudentScore
+{
+    partial class frmLogin
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblName = new System.Windows.Forms.Label();
+            this.txtName = new System.Windows.Forms.TextBox();
+            this.txtPwd = new System.Windows.Forms.TextBox();
+            this.btnOK = new System.Windows.Forms.Button();
+            this.btmnExit = new System.Windows.Forms.Button();
+            this.lblPwd = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // lblName
+            // 
+            this.lblName.AutoSize = true;
+            this.lblName.BackColor = System.Drawing.Color.Transparent;
+            this.lblName.Location = new System.Drawing.Point(155, 132);
+            this.lblName.Name = "lblName";
+            this.lblName.Size = new System.Drawing.Size(53, 12);
+            this.lblName.TabIndex = 0;
+            this.lblName.Text = "用户名：";
+            // 
+            // txtName
+            // 
+            this.txtName.Location = new System.Drawing.Point(214, 129);
+            this.txtName.Name = "txtName";
+            this.txtName.Size = new System.Drawing.Size(100, 21);
+            this.txtName.TabIndex = 1;
+            // 
+            // txtPwd
+            // 
+            this.txtPwd.Location = new System.Drawing.Point(214, 164);
+            this.txtPwd.Name = "txtPwd";
+            this.txtPwd.Size = new System.Drawing.Size(100, 21);
+            this.txtPwd.TabIndex = 2;
+            // 
+            // btnOK
+            // 
+            this.btnOK.Location = new System.Drawing.Point(148, 217);
+            this.btnOK.Name = "btnOK";
+            this.btnOK.Size = new System.Drawing.Size(60, 23);
+            this.btnOK.TabIndex = 3;
+            this.btnOK.Text = "登录";
+            this.btnOK.UseVisualStyleBackColor = true;
+            // 
+            // btmnExit
+            // 
+            this.btmnExit.Location = new System.Drawing.Point(262, 217);
+            this.btmnExit.Name = "btmnExit";
+            this.btmnExit.Size = new System.Drawing.Size(61, 23);
+            this.btmnExit.TabIndex = 4;
+            this.btmnExit.Text = "退出";
+            this.btmnExit.UseVisualStyleBackColor = true;
+            // 
+            // lblPwd
+            // 
+            this.lblPwd.AutoSize = true;
+            this.lblPwd.BackColor = System.Drawing.Color.Transparent;
+            this.lblPwd.Location = new System.Drawing.Point(167, 167);
+            this.lblPwd.Name = "lblPwd";
+            this.lblPwd.Size = new System.Drawing.Size(41, 12);
+            this.lblPwd.TabIndex = 5;
+            this.lblPwd.Text = "密码：";
+            // 
+            // frmLogin
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = global::StudentScore.Properties.Resources.login;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(449, 299);
+            this.Controls.Add(this.lblPwd);
+            this.Controls.Add(this.btmnExit);
+            this.Controls.Add(this.btnOK);
+            this.Controls.Add(this.txtPwd);
+            this.Controls.Add(this.txtName);
+            this.Controls.Add(this.lblName);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Name = "frmLogin";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "用户登录";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblName;
+        private System.Windows.Forms.TextBox txtName;
+        private System.Windows.Forms.TextBox txtPwd;
+        private System.Windows.Forms.Button btnOK;
+        private System.Windows.Forms.Button btmnExit;
+        private System.Windows.Forms.Label lblPwd;
+    }
+}
+
---
+++ StudentScore/StudentScore/frmMain.Designer.cs
@@ -0,0 +1,299 @@
+﻿namespace StudentScore
+{
+    partial class frmMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.学生信息管理TToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.课程信息管理CToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.成绩管理RToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.关于AToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.toolStrip1 = new System.Windows.Forms.ToolStrip();
+            this.toolStripButton1 = new System.Windows.Forms.ToolStripButton();
+            this.toolStripButton2 = new System.Windows.Forms.ToolStripButton();
+            this.toolStripButton3 = new System.Windows.Forms.ToolStripButton();
+            this.toolStripStatusLabel1 = new System.Windows.Forms.ToolStripStatusLabel();
+            this.学生信息查询ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.学生信息添加ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.学生信息修改ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.学生信息删除ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.课程信息查询ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.课程信息添加ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.课程信息修改ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.课程信息删除ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.成绩查询ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.成绩添加ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.成绩修改ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.成绩删除ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.版本ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip1.SuspendLayout();
+            this.statusStrip1.SuspendLayout();
+            this.toolStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.学生信息管理TToolStripMenuItem,
+            this.课程信息管理CToolStripMenuItem,
+            this.成绩管理RToolStripMenuItem,
+            this.关于AToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(1008, 25);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // 学生信息管理TToolStripMenuItem
+            // 
+            this.学生信息管理TToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.学生信息查询ToolStripMenuItem,
+            this.学生信息添加ToolStripMenuItem,
+            this.学生信息修改ToolStripMenuItem,
+            this.学生信息删除ToolStripMenuItem});
+            this.学生信息管理TToolStripMenuItem.Name = "学生信息管理TToolStripMenuItem";
+            this.学生信息管理TToolStripMenuItem.Size = new System.Drawing.Size(107, 21);
+            this.学生信息管理TToolStripMenuItem.Text = "学生信息管理(&T)";
+            // 
+            // 课程信息管理CToolStripMenuItem
+            // 
+            this.课程信息管理CToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.课程信息查询ToolStripMenuItem,
+            this.课程信息添加ToolStripMenuItem,
+            this.课程信息修改ToolStripMenuItem,
+            this.课程信息删除ToolStripMenuItem});
+            this.课程信息管理CToolStripMenuItem.Name = "课程信息管理CToolStripMenuItem";
+            this.课程信息管理CToolStripMenuItem.Size = new System.Drawing.Size(124, 21);
+            this.课程信息管理CToolStripMenuItem.Text = "课程信息管理（&C）";
+            // 
+            // 成绩管理RToolStripMenuItem
+            // 
+            this.成绩管理RToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.成绩查询ToolStripMenuItem,
+            this.成绩添加ToolStripMenuItem,
+            this.成绩修改ToolStripMenuItem,
+            this.成绩删除ToolStripMenuItem});
+            this.成绩管理RToolStripMenuItem.Name = "成绩管理RToolStripMenuItem";
+            this.成绩管理RToolStripMenuItem.Size = new System.Drawing.Size(84, 21);
+            this.成绩管理RToolStripMenuItem.Text = "成绩管理(&R)";
+            // 
+            // 关于AToolStripMenuItem
+            // 
+            this.关于AToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.版本ToolStripMenuItem});
+            this.关于AToolStripMenuItem.Name = "关于AToolStripMenuItem";
+            this.关于AToolStripMenuItem.Size = new System.Drawing.Size(60, 21);
+            this.关于AToolStripMenuItem.Text = "关于(&A)";
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripStatusLabel1});
+            this.statusStrip1.Location = new System.Drawing.Point(0, 707);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Size = new System.Drawing.Size(1008, 22);
+            this.statusStrip1.TabIndex = 1;
+            this.statusStrip1.Text = "statusStrip1";
+            // 
+            // toolStrip1
+            // 
+            this.toolStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripButton1,
+            this.toolStripButton2,
+            this.toolStripButton3});
+            this.toolStrip1.Location = new System.Drawing.Point(0, 25);
+            this.toolStrip1.Name = "toolStrip1";
+            this.toolStrip1.Size = new System.Drawing.Size(1008, 25);
+            this.toolStrip1.TabIndex = 2;
+            this.toolStrip1.Text = "toolStrip1";
+            // 
+            // toolStripButton1
+            // 
+            this.toolStripButton1.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.toolStripButton1.Image = global::StudentScore.Properties.Resources.student;
+            this.toolStripButton1.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.toolStripButton1.Name = "toolStripButton1";
+            this.toolStripButton1.Size = new System.Drawing.Size(23, 22);
+            this.toolStripButton1.Text = "toolStripButton1";
+            // 
+            // toolStripButton2
+            // 
+            this.toolStripButton2.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.toolStripButton2.Image = global::StudentScore.Properties.Resources.course;
+            this.toolStripButton2.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.toolStripButton2.Name = "toolStripButton2";
+            this.toolStripButton2.Size = new System.Drawing.Size(23, 22);
+            this.toolStripButton2.Text = "toolStripButton2";
+            // 
+            // toolStripButton3
+            // 
+            this.toolStripButton3.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.toolStripButton3.Image = global::StudentScore.Properties.Resources.score;
+            this.toolStripButton3.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.toolStripButton3.Name = "toolStripButton3";
+            this.toolStripButton3.Size = new System.Drawing.Size(23, 22);
+            this.toolStripButton3.Text = "toolStripButton3";
+            // 
+            // toolStripStatusLabel1
+            // 
+            this.toolStripStatusLabel1.Name = "toolStripStatusLabel1";
+            this.toolStripStatusLabel1.Size = new System.Drawing.Size(152, 17);
+            this.toolStripStatusLabel1.Text = "欢迎进入学生成绩管理系统";
+            // 
+            // 学生信息查询ToolStripMenuItem
+            // 
+            this.学生信息查询ToolStripMenuItem.Name = "学生信息查询ToolStripMenuItem";
+            this.学生信息查询ToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.学生信息查询ToolStripMenuItem.Text = "学生信息查询";
+            // 
+            // 学生信息添加ToolStripMenuItem
+            // 
+            this.学生信息添加ToolStripMenuItem.Name = "学生信息添加ToolStripMenuItem";
+            this.学生信息添加ToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.学生信息添加ToolStripMenuItem.Text = "学生信息添加";
+            // 
+            // 学生信息修改ToolStripMenuItem
+            // 
+            this.学生信息修改ToolStripMenuItem.Name = "学生信息修改ToolStripMenuItem";
+            this.学生信息修改ToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.学生信息修改ToolStripMenuItem.Text = "学生信息修改";
+            // 
+            // 学生信息删除ToolStripMenuItem
+            // 
+            this.学生信息删除ToolStripMenuItem.Name = "学生信息删除ToolStripMenuItem";
+            this.学生信息删除ToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.学生信息删除ToolStripMenuItem.Text = "学生信息删除";
+            // 
+            // 课程信息查询ToolStripMenuItem
+            // 
+            this.课程信息查询ToolStripMenuItem.Name = "课程信息查询ToolStripMenuItem";
+            this.课程信息查询ToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.课程信息查询ToolStripMenuItem.Text = "课程信息查询";
+            // 
+            // 课程信息添加ToolStripMenuItem
+            // 
+            this.课程信息添加ToolStripMenuItem.Name = "课程信息添加ToolStripMenuItem";
+            this.课程信息添加ToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.课程信息添加ToolStripMenuItem.Text = "课程信息添加";
+            // 
+            // 课程信息修改ToolStripMenuItem
+            // 
+            this.课程信息修改ToolStripMenuItem.Name = "课程信息修改ToolStripMenuItem";
+            this.课程信息修改ToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.课程信息修改ToolStripMenuItem.Text = "课程信息修改";
+            // 
+            // 课程信息删除ToolStripMenuItem
+            // 
+            this.课程信息删除ToolStripMenuItem.Name = "课程信息删除ToolStripMenuItem";
+            this.课程信息删除ToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.课程信息删除ToolStripMenuItem.Text = "课程信息删除";
+            // 
+            // 成绩查询ToolStripMenuItem
+            // 
+            this.成绩查询ToolStripMenuItem.Name = "成绩查询ToolStripMenuItem";
+            this.成绩查询ToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.成绩查询ToolStripMenuItem.Text = "成绩查询";
+            // 
+            // 成绩添加ToolStripMenuItem
+            // 
+            this.成绩添加ToolStripMenuItem.Name = "成绩添加ToolStripMenuItem";
+            this.成绩添加ToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.成绩添加ToolStripMenuItem.Text = "成绩添加";
+            // 
+            // 成绩修改ToolStripMenuItem
+            // 
+            this.成绩修改ToolStripMenuItem.Name = "成绩修改ToolStripMenuItem";
+            this.成绩修改ToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.成绩修改ToolStripMenuItem.Text = "成绩修改";
+            // 
+            // 成绩删除ToolStripMenuItem
+            // 
+            this.成绩删除ToolStripMenuItem.Name = "成绩删除ToolStripMenuItem";
+            this.成绩删除ToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.成绩删除ToolStripMenuItem.Text = "成绩删除";
+            // 
+            // 版本ToolStripMenuItem
+            // 
+            this.版本ToolStripMenuItem.Name = "版本ToolStripMenuItem";
+            this.版本ToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.版本ToolStripMenuItem.Text = "版本";
+            // 
+            // frmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1008, 729);
+            this.Controls.Add(this.toolStrip1);
+            this.Controls.Add(this.statusStrip1);
+            this.Controls.Add(this.menuStrip1);
+            this.IsMdiContainer = true;
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "frmMain";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "学生成绩管理系统";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.statusStrip1.ResumeLayout(false);
+            this.statusStrip1.PerformLayout();
+            this.toolStrip1.ResumeLayout(false);
+            this.toolStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem 学生信息管理TToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 课程信息管理CToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 成绩管理RToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 关于AToolStripMenuItem;
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.ToolStrip toolStrip1;
+        private System.Windows.Forms.ToolStripButton toolStripButton1;
+        private System.Windows.Forms.ToolStripButton toolStripButton2;
+        private System.Windows.Forms.ToolStripButton toolStripButton3;
+        private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabel1;
+        private System.Windows.Forms.ToolStripMenuItem 学生信息查询ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 学生信息添加ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 学生信息修改ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 学生信息删除ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 课程信息查询ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 课程信息添加ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 课程信息修改ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 课程信息删除ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 成绩查询ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 成绩添加ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 成绩修改ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 成绩删除ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 版本ToolStripMenuItem;
+    }
+}
